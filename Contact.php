f41ee3f9bf531bb58caeae671d237b55ad2c11d0
Allam <92165435+mahmoud530@users.noreply.github.com>
2025-02-24 02:04:04 +0200

    Update Contact.php

--- Contact.php
+++ Contact.php
@@ -11,6 +11,8 @@ include './Insert.php';
   <link rel="stylesheet" href="./css/all.min.css"> 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" href="./img/SCCI_Logo.png">
+
   <link
     href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Literata:ital,opsz,wght@0,7..72,200..900;1,7..72,200..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
     rel="stylesheet">
